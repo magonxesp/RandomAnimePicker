787eac3d94e9aa07e682c3ed7da8b185512682db
MagonxESP <janma.360@gmail.com>
2020-02-15 12:02:28 +0100

    Updates

---
+++ precache-manifest.dd1880c5b9b59fc0e3f4fa86310271d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb2603f246ad468b2c8edb49e27ce6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1428a641a4cc21ab5e75",
+    "url": "/static/css/main.bc8f4af7.chunk.css"
+  },
+  {
+    "revision": "2a5b12b206fd83738b90",
+    "url": "/static/js/2.f6bcc467.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.f6bcc467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1428a641a4cc21ab5e75",
+    "url": "/static/js/main.6af2cc0d.chunk.js"
+  },
+  {
+    "revision": "f075bebe111449b4b3cb",
+    "url": "/static/js/runtime-main.d3a81e60.js"
+  },
+  {
+    "revision": "28d180f9cdc1947b6ffe1800c6e195f8",
+    "url": "/static/media/loading.28d180f9.gif"
+  }
+]);
