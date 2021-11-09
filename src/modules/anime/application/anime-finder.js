c82b506b90ebbe07ed5dbc4c05428c72aca8e846
MagonxESP <janma.360@gmail.com>
2021-11-09 01:00:47 +0100

    remove unused comments

--- src/modules/anime/application/anime-finder.js
+++ src/modules/anime/application/anime-finder.js
@@ -18,47 +18,4 @@ export default class AnimeFinder {
         return await this.repository.random();
     }
 
-    // #season = [
-    //     'summer',
-    //     'spring',
-    //     'fall',
-    //     'winter'
-    // ];
-    //
-    // constructor() {
-    //     this.api = jikanjs
-    // }
-    //
-    // getRandomInt(min, max) {
-    //     min = Math.ceil(min);
-    //     max = Math.floor(max);
-    //     return Math.floor(Math.random() * (max - min + 1)) + min;
-    // }
-    //
-    // randomSeason() {
-    //     return this.#season[this.getRandomInt(0, this.#season.length - 1)]
-    // }
-    //
-    // async getAnime(id) {
-    //     try {
-    //         return await jikanjs.loadAnime(id);
-    //     } catch (exception) {
-    //         return null;
-    //     }
-    // }
-    //
-    // async random() {
-    //     try {
-    //         let now = new Date();
-    //         let year = this.getRandomInt(now.getFullYear() - 10, now.getFullYear());
-    //         let season = this.randomSeason();
-    //         let response = await jikanjs.loadSeason(year, season);
-    //         let anime = response['anime-detail.vue'][this.getRandomInt(0, response['anime-detail.vue'].length - 1)];
-    //         return await this.getAnime(anime['mal_id']);
-    //     } catch (exception) {
-    //         console.log(exception);
-    //         return null;
-    //     }
-    // }
-
 }
