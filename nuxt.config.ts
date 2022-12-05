a7dd325efd61f731c11e953bcc4d857a15936808
MagonxESP <janma.360@gmail.com>
2022-12-05 02:05:26 +0100

    ssr true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	ssr: false,
 	css: [
 		"@fortawesome/fontawesome-svg-core/styles.css",
 		"@/assets/styles/main.scss",
